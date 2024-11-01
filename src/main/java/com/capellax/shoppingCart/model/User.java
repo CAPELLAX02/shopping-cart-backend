0931f62438c83910be4e4d3fa02e5534a9895cf7
CAPELLAX02 <ahmtatar@gmail.com>
2024-11-01 10:52:09 +0300

    create Cart & CartItem & User entities and set up the relationships between them

---
+++ src/main/java/com/capellax/shoppingCart/model/User.java
@@ -0,0 +1,36 @@
+package com.capellax.shoppingCart.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.query.Order;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @NaturalId
+    private String email;
+
+    private String password;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Cart cart;
+
+}
