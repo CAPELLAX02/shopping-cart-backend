ba86aca9b5203eab21db71d0c7b49ee34e720be7
CAPELLAX02 <ahmtatar@gmail.com>
2024-11-01 14:12:01 +0300

    cart and cartItem service implementations

--- src/main/java/com/capellax/shoppingCart/service/cart/cartItem/CartItemService.java
+++ src/main/java/com/capellax/shoppingCart/service/cart/cartItem/CartItemService.java
@@ -1,4 +1,96 @@
 package com.capellax.shoppingCart.service.cart.cartItem;
 
-public class CartItemService {
+import com.capellax.shoppingCart.exceptions.ResourceNotFoundException;
+import com.capellax.shoppingCart.model.Cart;
+import com.capellax.shoppingCart.model.CartItem;
+import com.capellax.shoppingCart.model.Product;
+import com.capellax.shoppingCart.repository.CartItemRepository;
+import com.capellax.shoppingCart.repository.CartRepository;
+import com.capellax.shoppingCart.service.cart.ICartService;
+import com.capellax.shoppingCart.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class CartItemService implements ICartItemService {
+
+    private final CartItemRepository cartItemRepository;
+    private final CartRepository cartRepository;
+    private final ICartService iCartService;
+    private final IProductService iProductService;
+
+    @Override
+    public void addItemToCart(
+            Long cartId,
+            Long productId,
+            int quantity
+    ) {
+        Cart cart = iCartService.getCart(cartId);
+        Product product = iProductService.getProductById(productId);
+        CartItem cartItem = cart.getItems()
+                .stream()
+                .filter(item -> item.getId().equals(productId))
+                .findFirst()
+                .orElse(new CartItem());
+        if (cartItem.getId() == null) {
+            cartItem.setCart(cart);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(quantity);
+            cartItem.setUnitPrice(product.getPrice());
+        } else {
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        }
+        cartItem.setTotalPrice();
+        cart.addItem(cartItem);
+        cartItemRepository.save(cartItem);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public void removeItemFromCart(
+            Long cartId,
+            Long productId
+    ) {
+        Cart cart = iCartService.getCart(cartId);
+        CartItem itemToRemove = getCartItem(cartId, productId);
+        cart.removeItem(itemToRemove);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public void updateItemQuantity(
+            Long cartId,
+            Long productId,
+            int quantity
+    ) {
+        Cart cart = iCartService.getCart(cartId);
+        cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .ifPresent(item -> {
+                    item.setQuantity(quantity);
+                    item.setUnitPrice(item.getProduct().getPrice());
+                    item.setTotalPrice();
+                });
+        BigDecimal totalAmount = cart.getTotalAmount();
+        cart.setTotalAmount(totalAmount);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public CartItem getCartItem(
+            Long cartId,
+            Long productId
+    ) {
+        Cart cart = iCartService.getCart(cartId);
+        return cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+    }
 }
--- src/main/java/com/capellax/shoppingCart/service/cart/cartItem/ICartItemService.java
+++ src/main/java/com/capellax/shoppingCart/service/cart/cartItem/ICartItemService.java
@@ -1,4 +1,12 @@
 package com.capellax.shoppingCart.service.cart.cartItem;
 
+import com.capellax.shoppingCart.model.CartItem;
+
 public interface ICartItemService {
+
+    void addItemToCart(Long cartId, Long productId, int quantity);
+    void removeItemFromCart(Long cartId, Long productId);
+    void updateItemQuantity(Long cartId, Long productId, int quantity);
+
+    CartItem getCartItem(Long cartId, Long productId);
 }
